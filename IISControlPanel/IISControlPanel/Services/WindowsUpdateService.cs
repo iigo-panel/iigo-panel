84efe08c7985af346594b3eaded77ab0373173ad
Adam Humpherys <adam@adamh.us>
2023-03-22 11:09:29 -0600

    Build fixes, added first iteration of WindowsUpdateService

---
+++ IISControlPanel/IISControlPanel/Services/WindowsUpdateService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WUApiLib;
+
+namespace IISControlPanel.Services
+{
+    public class WindowsUpdateService
+    {
+        public async Task<UpdateCollection> GetUpdates()
+        {
+            return await Task.Run(() =>
+            {
+                UpdateSession uSession = new UpdateSession();
+                IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+                ISearchResult uResult = uSearcher.Search("IsInstalled=0 and Type = 'Software'");
+
+                return uResult.Updates;
+            });
+        }
+
+        public async Task DownloadUpdates(UpdateCollection updates)
+        {
+            await Task.Run(() =>
+            {
+                UpdateSession uSession = new UpdateSession();
+                UpdateDownloader downloader = uSession.CreateUpdateDownloader();
+                downloader.Updates = updates;
+                downloader.Download();
+            });
+        }
+    }
+}
