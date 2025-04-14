242c36020fd11ed34461d4c919de1df7e813fe5c
Adam <adam@adamh.us>
2025-04-14 11:48:18 -0600

    added version to footer

--- IIGO/Services/Constants.cs
+++ IIGO/Services/Constants.cs
@@ -4,6 +4,6 @@
     {
         public const string EventLogSource = "IIGO";
         public const string EventLogName= "IIGO";
-        public const string IIGOVersion = "1.0.0";
+        public const string IIGOVersion = "1.0-beta2";
     }
 }
