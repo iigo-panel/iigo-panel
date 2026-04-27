1aec8c30d4994d2357f92e8ebc586ecd72d941de
Adam <adam@adamh.us>
2026-04-27 14:03:32 -0600

    version bump

--- IIGO/Services/Constants.cs
+++ IIGO/Services/Constants.cs
@@ -4,7 +4,7 @@
     {
         public const string EventLogSource = "IIGO";
         public const string EventLogName= "IIGO";
-        public const string IIGOVersion = "1.0.0-beta2";
-        public readonly static DateTime BuildDate = DateTime.Parse("2025/09/13");
+        public const string IIGOVersion = "1.0.0-beta3";
+        public readonly static DateTime BuildDate = DateTime.Parse("2026/04/27");
     }
 }
